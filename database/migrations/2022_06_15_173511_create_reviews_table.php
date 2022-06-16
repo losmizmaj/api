50bdd2ffeb10ed723b3828db7cd6bf2e212efa89
losmizmaj <milossegedi23@gmail.com>
2022-06-16 04:21:52 -0700

    revise

--- database/migrations/2022_06_15_173511_create_reviews_table.php
+++ database/migrations/2022_06_15_173511_create_reviews_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('product_id')->constraindes()->onDelete('cascade');
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
             $table->string('customer');
             $table->text('review');
             $table->integer('star');
